3cd0e7b2ee8ce1ffad71460b1bd39e4a4daf84b2
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-01-25 16:07:44 +0300

    add postgres

--- identity/src/main/java/ru/taskmanager/identity/IdentityApplication.java
+++ identity/src/main/java/ru/taskmanager/identity/IdentityApplication.java
@@ -2,13 +2,8 @@ package ru.taskmanager.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class IdentityApplication {
 
 	public static void main(String[] args) {
