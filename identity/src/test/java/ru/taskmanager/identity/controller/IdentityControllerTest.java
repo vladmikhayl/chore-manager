1f0879b4ac5563ed2b1683a0a9190dfcc4e88ef2
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-02-07 23:59:54 +0300

    add identity integration tests

--- identity/src/test/java/ru/taskmanager/identity/controller/IdentityControllerTest.java
+++ identity/src/test/java/ru/taskmanager/identity/controller/IdentityControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-public class IdentityControllerTests {
+public class IdentityControllerTest {
     private MockMvc mockMvc;
 
     private ObjectMapper objectMapper;
