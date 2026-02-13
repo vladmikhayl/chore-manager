3c9ebabc6161bc490e56c47c28c3b113e050c6cb
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-02-13 03:11:33 +0300

    fix tests config

--- identity/src/test/java/ru/taskmanager/identity/integration/IdentityIntegrationTest.java
+++ identity/src/test/java/ru/taskmanager/identity/integration/IdentityIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.taskmanager.identity.dto.request.LoginRequest;
 import ru.taskmanager.identity.dto.request.NotificationSettingsRequest;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestPropertySource(properties = { "spring.config.location=classpath:/application-test.yml" })
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IdentityIntegrationTest {
