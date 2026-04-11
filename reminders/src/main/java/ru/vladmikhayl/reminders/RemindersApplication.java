b9271920d1ae0e8eff3037cb0de617223ca27f8f
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-04-11 23:19:09 +0300

    init reminders service

---
+++ reminders/src/main/java/ru/vladmikhayl/reminders/RemindersApplication.java
@@ -0,0 +1,13 @@
+package ru.vladmikhayl.reminders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindersApplication.class, args);
+	}
+
+}
