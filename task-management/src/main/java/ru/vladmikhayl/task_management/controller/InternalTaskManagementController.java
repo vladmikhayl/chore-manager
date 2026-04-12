909b11bdcd1dcd1d225e2ec189e7385531bc725a
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-04-12 21:01:40 +0300

    add internal endpoint for getting tasks for day

--- task-management/src/main/java/ru/vladmikhayl/task_management/controller/InternalTaskManagementController.java
+++ task-management/src/main/java/ru/vladmikhayl/task_management/controller/InternalTaskManagementController.java
@@ -2,19 +2,25 @@ package ru.vladmikhayl.task_management.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vladmikhayl.task_management.dto.request.TasksForUsersRequest;
+import ru.vladmikhayl.task_management.dto.response.TaskResponse;
 import ru.vladmikhayl.task_management.dto.response.UserTasksForReminderResponse;
 import ru.vladmikhayl.task_management.service.InternalTaskManagementService;
+import ru.vladmikhayl.task_management.service.TaskManagementService;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/internal")
 @RequiredArgsConstructor
 @Hidden
 public class InternalTaskManagementController {
+    private final TaskManagementService taskManagementService;
     private final InternalTaskManagementService internalTaskManagementService;
 
     @PostMapping("/tasks-for-users")
@@ -27,5 +33,13 @@ public class InternalTaskManagementController {
         );
 
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<TaskResponse>> getTasksForDay(
+            @RequestHeader("X-User-Id") UUID userId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        return ResponseEntity.ok(taskManagementService.getTasksForDay(userId, date));
     }
 }
