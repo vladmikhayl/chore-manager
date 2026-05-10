9312195ef96dda1401deeb314acfda9c5c395696
vladmikhayl <vamikhaylov_2@edu.hse.ru>
2026-05-10 19:26:48 +0300

    add auto logout after jwt expiring

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getAccessToken } from "../utils/authStorage";
+import { getAccessToken, removeAccessToken } from "../utils/authStorage";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
@@ -19,3 +19,22 @@ apiClient.interceptors.request.use((config) => {
 
   return config;
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      removeAccessToken();
+
+      const currentPath = window.location.pathname + window.location.search;
+
+      const loginUrl = `/login?redirect=${encodeURIComponent(currentPath)}`;
+
+      if (window.location.pathname !== "/login") {
+        window.location.href = loginUrl;
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
